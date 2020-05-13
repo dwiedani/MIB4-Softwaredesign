83ec5d93132b2aa3f6a657af851789d129e5e6d6
Cornelius Schaub <github@cornelius-schaub.com>
2020-05-13 10:28:40 +0200

    making Code review with Comments

--- L02 - Leandro Knecht/src/assets/ts/main.ts
+++ L02 - Leandro Knecht/src/assets/ts/main.ts
@@ -1,4 +1,5 @@
-
+//überall sollte " anstatt ' benutzt werden
+//"subject" ist in der einzahl, die anderen Arrays in der Mehrzahl
 const subject: string[] = ['Hyur', 'Elezen', 'Lalafell', 'Miqo\'te',
   'Roegadyn', 'Au Ra', 'Viera', 'Hrothgar'];
 const verbs: string[] = ['lieben', 'hassen', 'können',
@@ -9,6 +10,7 @@ const objects: string[] = ['Lebensmittel', 'die Ultima-Weapon',
 
 function randomRiddle() {
   for (let i = 0; i < 5; i++) {
+    //sehr lange Zeile. Lässt sich nur schwer lesen und sollte übersichtlich aufgespaltet werden
     console.log(subject.splice(Math.floor(Math.random() * subject.length), 1)[0] + verbs.splice(Math.floor(Math.random() * verbs.length), 1)[0] + objects.splice(Math.floor(Math.random() * objects.length), 1)[0], '.', '\n');
   }
 }
