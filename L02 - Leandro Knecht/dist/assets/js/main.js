9e4ccc3571e043510ba2b821c513b500341edc81
Daniel Schlegel <d.schlegel@mediadudes.de>
2020-05-13 10:46:02 +0200

    fix code due to coding standard issues

--- L02 - Leandro Knecht/dist/assets/js/main.js
+++ L02 - Leandro Knecht/dist/assets/js/main.js
@@ -1,4 +1,6 @@
-var subject = ['Hyur', 'Elezen', 'Lalafell', 'Miqo\'te',
+// überall sollte " anstatt " benutzt werden
+// "subject" ist in der einzahl, die anderen Arrays in der Mehrzahl
+var subject = ['Hyur', 'Elezen', 'Lalafell', "Miqo'te",
     'Roegadyn', 'Au Ra', 'Viera', 'Hrothgar'];
 var verbs = ['lieben', 'hassen', 'können',
     'zerstören', 'kämpfen', 'lachen'];
@@ -6,7 +8,10 @@ var objects = ['Lebensmittel', 'die Ultima-Weapon',
     'Gridania', 'Nero', 'Midgardsormr', 'Gegner'];
 function randomRiddle() {
     for (var i = 0; i < 5; i++) {
-        console.log(subject.splice(Math.floor(Math.random() * subject.length), 1)[0] + verbs.splice(Math.floor(Math.random() * verbs.length), 1)[0] + objects.splice(Math.floor(Math.random() * objects.length), 1)[0], '.', '\n');
+        // sehr lange Zeile. Lässt sich nur schwer lesen und sollte übersichtlich aufgespaltet werden
+        console.log(subject.splice(Math.floor(Math.random() * subject.length), 1)[0]
+            + verbs.splice(Math.floor(Math.random() * verbs.length), 1)[0]
+            + objects.splice(Math.floor(Math.random() * objects.length), 1)[0], '.', '\n');
     }
 }
 randomRiddle();
--- L02 - Leandro Knecht/src/assets/ts/main.ts
+++ L02 - Leandro Knecht/src/assets/ts/main.ts
@@ -1,6 +1,6 @@
-//überall sollte " anstatt ' benutzt werden
-//"subject" ist in der einzahl, die anderen Arrays in der Mehrzahl
-const subject: string[] = ['Hyur', 'Elezen', 'Lalafell', 'Miqo\'te',
+// überall sollte " anstatt " benutzt werden
+// "subject" ist in der einzahl, die anderen Arrays in der Mehrzahl
+const subject: string[] = ['Hyur', 'Elezen', 'Lalafell', "Miqo'te",
   'Roegadyn', 'Au Ra', 'Viera', 'Hrothgar'];
 const verbs: string[] = ['lieben', 'hassen', 'können',
   'zerstören', 'kämpfen', 'lachen'];
@@ -10,8 +10,13 @@ const objects: string[] = ['Lebensmittel', 'die Ultima-Weapon',
 
 function randomRiddle() {
   for (let i = 0; i < 5; i++) {
-    //sehr lange Zeile. Lässt sich nur schwer lesen und sollte übersichtlich aufgespaltet werden
-    console.log(subject.splice(Math.floor(Math.random() * subject.length), 1)[0] + verbs.splice(Math.floor(Math.random() * verbs.length), 1)[0] + objects.splice(Math.floor(Math.random() * objects.length), 1)[0], '.', '\n');
+    // sehr lange Zeile. Lässt sich nur schwer lesen und sollte übersichtlich aufgespaltet werden
+    console.log(
+      subject.splice(Math.floor(Math.random() * subject.length), 1)[0]
+        + verbs.splice(Math.floor(Math.random() * verbs.length), 1)[0]
+        + objects.splice(Math.floor(Math.random() * objects.length), 1)[0],
+      '.', '\n',
+    );
   }
 }
 
