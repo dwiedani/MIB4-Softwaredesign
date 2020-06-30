34cbd28d5fe9faced10c3bfdcf8549dbb66e5283
Daniel Schlegel <d.schlegel@mediadudes.de>
2020-06-30 22:44:05 +0200

    remove ja files

--- L03 - Quiz/src/assets/ts/components/Question.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var Question = /** @class */ (function () {
-    function Question(_question) {
-        this.question = _question;
-    }
-    return Question;
-}());
-module.exports = Question;
